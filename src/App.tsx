ab05fa51ea25584e962b2da35b084c26218433f2
Joel Fragoso <joelfragoso85@icloud.com>
2020-08-28 15:53:36 -0300

    feat: add localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,30 @@ import darkTheme from './styles/themes/dark';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
+    const theme = localStorage.getItem('@DarkMode:theme');
+
+    if (theme) {
+      const { dark } = JSON.parse(theme);
+
+      return !!dark;
+    }
+
+    return false;
+  });
 
   return (
     <>
       <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
         <Dashboard
           isDarkMode={isDarkMode}
-          onDarkMode={() => setIsDarkMode(!isDarkMode)}
+          onDarkMode={() => {
+            setIsDarkMode(!isDarkMode);
+            localStorage.setItem(
+              '@DarkMode:theme',
+              JSON.stringify({ dark: !isDarkMode }),
+            );
+          }}
         />
       </ThemeProvider>
       <GlobalStyle />
